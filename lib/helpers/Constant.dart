710fe7a0eccb2335ab77b3f4d72929ab6982f4bc
Aviral Sharma <aviral2020it015@abesit.edu.in>
2022-10-30 04:26:43 -0700

    initir

---
+++ lib/helpers/Constant.dart
@@ -0,0 +1,61 @@
+const appName = 'MSocial';
+
+const String appbartitle = 'MSocial';
+const String andoidPackageName = 'com.mothering.social ';
+
+//change this url to set your URL in app
+String webinitialUrl = 'https://www.msocialin.com';
+const String firstTabUrl = 'https://www.google.com';
+
+//keep local content of pages of setting screen
+const String aboutPageURL = 'https://msocialin.com/terms/about-us';
+const String privacyPageURL = 'https://msocialin.com/terms/privacy-policy';
+const String termsPageURL = 'https://msocialin.com/terms/terms';
+
+//Change App id of android and IOS app
+const String androidAppId = andoidPackageName;
+
+const String iOSAppId = 'ZF3Y932DXT.com.mothering.social';
+
+const String shareAppTitle = appName;
+const String shareiOSAppMessage = '';
+const String shareAndroidAppMessge =
+    'Download $appName App from this link : http://play.google.com/store/apps/details?id=$androidAppId';
+
+//To turn on/off ads
+const bool showInterstitialAds = false;
+const bool showBannerAds = false;
+const bool showOpenAds = false;
+
+//To turn on/off display of bottom navigation bar
+const bool showBottomNavigationBar = false;
+
+//To show/remove splash screen
+const bool showSplashScreen = false;
+
+//To remove/display header/footer of website
+const bool hideHeader = false;
+const bool hideFooter = false;
+
+//Ids for interstitial Ad
+const androidInterstitialAdId = 'ca-app-pub-3940256099942544/1033173712';
+const iosInterstitialAdId = 'ca-app-pub-3940256099942544/4411468910';
+
+//Ids for banner Ad
+const androidBannerAdId = 'ca-app-pub-3940256099942544/6300978111';
+const iosBannerAdId = 'ca-app-pub-3940256099942544/2934735716';
+
+//Ids for app open Ad
+const androidOpenAdId = 'ca-app-pub-3940256099942544/3419835294';
+const iosOpenAdId = 'ca-app-pub-3940256099942544/5662855259';
+
+//icon to set when get firebase messages
+const String messageIcon = '@mipmap/ic_launcher_squircle';
+
+//copy your  one-signal android app id
+const oneSignalAndroidAppId = 'b7137cc0-c5f5-4029-a442-c02e5153c931';
+
+//copy your  one-signal ios app id
+const oneSignalIOSAppId = 'b7137cc0-c5f5-4029-a442-c02e5153c931';
+
+const String iconPath = 'assets/icons/';
